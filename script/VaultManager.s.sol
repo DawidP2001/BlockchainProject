afaae31967ed548a35e5afa5b6480ac4977909b4
DawidP2001 <c00273530@setu.ie>
2025-04-19 13:35:10 +0100

    Changed the script file

--- script/VaultManager.s.sol
+++ script/VaultManager.s.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {TaskManager} from "../src/TaskManager.sol";
+import {VaultManager} from "../src/VaultManager.sol";
 
-contract TaskManagerScript is Script {
+contract VaultManagerScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        TaskManager tm = new TaskManager();
+        VaultManager vManager = new VaultManager();
         vm.stopBroadcast();
     }
 }
